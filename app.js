cc455099951818ed99f5fe223963b9183ab140cd
Marianna Guedez <141771904+GueDevz@users.noreply.github.com>
2024-08-25 16:54:25 -0300

    Fixs (#9)
    * fix(js): Crea una nueva carpeta js y se elimina el modo oscuro.
    
    * fix(encrypt/decrypt): Elimina un algoritmo de encriptacion ya que genera errores en la desencriptacion.

--- app.js
+++ app.js
@@ -1,70 +1,3 @@
-/* ==============================-------------------- HOME PAGE TITLE ANIMATION--------------------============================== */
-
-document.addEventListener('DOMContentLoaded', () => {
-    const textElement = document.getElementById('hero__title-reveal');
-    const fullText = 'ENCRYPTIFY';
-    const characters = '!1@2#3$4%5^6&7*8(9)0_+-=[]{}|;:>' + fullText;
-    const revealDuration = 600;
-    const pauseDuration = 1500;
-    const revealIntervalDuration = revealDuration / 8;
-    const numCycles = 10;
-
-    const currentText = Array(fullText.length).fill('');
-
-    function revealCharacter(index, cycle = 0) {
-        if (cycle < numCycles) {
-            currentText[index] = generateRandomCharacter();
-            textElement.textContent = currentText.join('');
-            setTimeout(() => revealCharacter(index, cycle + 1), revealIntervalDuration);
-        } else {
-            currentText[index] = fullText[index];
-            textElement.textContent = currentText.join('');
-            if (index < fullText.length - 1) {
-                setTimeout(() => revealCharacter(index + 1), revealDuration);
-            } else {
-                setTimeout(startReveal, pauseDuration);
-            }
-        }
-    }
-
-    function generateRandomCharacter() {
-        return characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-    function startReveal() {
-        currentText.fill('');
-        revealCharacter(0);
-    }
-
-    startReveal();
-});
-
-
-/* ==============================-------------------- BUTTON DARK/LIGHT MODE --------------------============================== */
-
-document.addEventListener('DOMContentLoaded', () => {
-    const switcher = document.querySelector('.header__button');
-    const switchIcon = document.querySelector('.header__button-icon');
-    const logo = document.querySelector('.header__logo');
-    const body = document.querySelector('body');
-
-    switcher.addEventListener('click', () => {
-        body.classList.toggle('active');
-
-        if (body.classList.contains('active')) {
-            switchIcon.classList.replace('bx-sun', 'bx-moon');
-            body.style.backgroundImage = 'url(../assets/images/background-2.jpg)';
-            logo.src = "../assets/images/Logo-dark.svg";
-
-        } else {
-            switchIcon.classList.replace('bx-moon', 'bx-sun');
-            body.style.backgroundImage = 'url(../assets/images/background.jpg)';
-            logo.src = "../assets/images/Logo-light.svg";
-        }
-    });
-});
-
-
 /* ==============================-------------------- DROPDOWN TO CHOOSE ENCRYPTION ALGORITHM --------------------============================== */
 
 function dropdown() {
@@ -268,27 +201,6 @@ function cesarEncryptionMethod(text, move) {
     return cipherText;
 }
 
-function vigenereEncryptionMethod(text, key) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    text = text.toLowerCase().replace(/\s/g, '');
-    key = key.toLowerCase().repeat(Math.ceil(text.length / key.length));
-
-    let cipherText = '';
-
-    for (let i = 0; i < text.length; i++) {
-        if (alphabet.includes(text[i])) {
-            let textIndex = alphabet.indexOf(text[i]);
-            let keyIndex = alphabet.indexOf(key[i]);
-            let newIndex = (textIndex + keyIndex) % 26;
-            cipherText += alphabet[newIndex];
-        } else {
-            cipherText += text[i];
-        }
-    }
-
-    return cipherText;
-}
-
 
 /* ==============================-------------------- ENCRYPT BUTTON FUNCTION |SECTION ENCRYPT|--------------------============================== */
 function encryptBtn() {
@@ -314,7 +226,7 @@ function encryptBtn() {
 
         message.value = encryptedText;
         message.style.backgroundImage = "none";
-        // textArea.value = '';
+        textArea.value = '';
     }
 }
 
@@ -435,27 +347,6 @@ function cesarDecryptionMethod(text, move) {
     return decryptedText;
 }
 
-function vigenereDecryptionMethod(text, key) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    text = text.toLowerCase().replace(/\s/g, '');
-    key = key.toLowerCase().repeat(Math.ceil(text.length / key.length));
-
-    let decryptedText = '';
-
-    for (let i = 0; i < text.length; i++) {
-        if (alphabet.includes(text[i])) {
-            let textIndex = alphabet.indexOf(text[i]);
-            let keyIndex = alphabet.indexOf(key[i]);
-            let newIndex = (textIndex - keyIndex + 26) % 26;
-            decryptedText += alphabet[newIndex];
-        } else {
-            decryptedText += text[i];
-        }
-    }
-
-    return decryptedText;
-}
-
 
 /* ==============================-------------------- DECRYPT BUTTON FUNCTION |SECTION DECRYPT|--------------------============================== */
 
@@ -480,9 +371,6 @@ function decryptBtn() {
     } else if (dropdownTitle === "César") {
         const shift = 3;
         decryptedText = cesarDecryptionMethod(message.value, shift);
-    } else if (dropdownTitle === "Vigenère") {
-        const key = inputPassword.value.trim();
-        decryptedText = vigenereDecryptionMethod(message.value, key);
     } else {
         return;
     }
